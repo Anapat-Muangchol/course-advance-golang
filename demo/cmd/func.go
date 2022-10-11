8086c50fb01d58255436c15a166045cd629282b0
Anapat Muangchol <anapat.mua@trueditital.com>
2022-10-11 10:41:00 +0700

    split to function

---
+++ demo/cmd/func.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"demo"
+	"fmt"
+)
+
+func sayHi() {
+	m, err := demo.SayHi()
+	if err == nil {
+		fmt.Println(m)
+	}
+}
+
+func loop() {
+	// Loop :: for
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	// Loop :: while
+	i := 1
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}
+
+	// Infinity Loop
+	//for {}
+
+	m, err := demo.SayHi()
+	if err == nil {
+		fmt.Println(m)
+	}
+}
--- demo/cmd/main.go
+++ demo/cmd/main.go
@@ -1,28 +1,6 @@
 package main
 
-import (
-	"demo"
-	"fmt"
-)
-
 func main() {
-	// Loop :: for
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-
-	// Loop :: while
-	i := 1
-	for i < 5 {
-		fmt.Println(i)
-		i++
-	}
-
-	// Infinity Loop
-	//for {}
-
-	m, err := demo.SayHi()
-	if err == nil {
-		fmt.Println(m)
-	}
+	sayHi()
+	loop()
 }
