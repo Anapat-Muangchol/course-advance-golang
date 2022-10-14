e702f5e835ac43cdcc0fac46ff0b6ddb2887e5b6
Anapat Muangchol <anapat.mua@trueditital.com>
2022-10-14 18:27:18 +0700

    add zipkin

--- rest_api_echo/main.go
+++ rest_api_echo/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"api/internal"
 	"api/users"
+	"context"
 	"flag"
 	"fmt"
 	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"log"
 	"net/http"
 	"os"
 )
@@ -17,8 +21,20 @@ var port = flag.String("port", "1323", "Server Port")
 var url = flag.String("url", "mongodb://root:password@localhost:27017", "Host Name MongoDb")
 
 func main() {
-	fmt.Println(os.Getenv("GOPATH"))
 
+	// Init tracer
+	tp, err := internal.InitTracer("http://zipkin:9411/api/v2/spans", "demo-api")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(tp *sdktrace.TracerProvider, ctx context.Context) {
+		err := tp.Shutdown(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(tp, context.Background())
+
+	fmt.Println(os.Getenv("GOPATH"))
 	flag.Parse()
 
 	// Echo server
@@ -28,6 +44,7 @@ func main() {
 	p := prometheus.NewPrometheus("echo", nil)
 	p.Use(e)
 	e.Use(middleware.Recover())
+	e.Use(otelecho.Middleware("demo-api"))
 
 	// Routers
 	e.GET("/", homeHandler)
--- rest_api_echo/users/user.go
+++ rest_api_echo/users/user.go
@@ -2,6 +2,9 @@ package users
 
 import (
 	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	oteltrace "go.opentelemetry.io/otel/trace"
 	"net/http"
 )
 
@@ -9,8 +12,12 @@ type UserResponse struct {
 	Message string `json:"message"`
 }
 
+var tracer = otel.Tracer("demo-api")
+
 func GetUserHandler(service *UserService) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		_, span := tracer.Start(c.Request().Context(), "GetUserHandler", oteltrace.WithAttributes(attribute.String("layer", "handler")))
+		defer span.End()
 		res := UserResponse{
 			Message: service.GetAll(),
 		}
